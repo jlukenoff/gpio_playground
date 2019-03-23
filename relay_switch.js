e43d1f8de257ada38e0fdc0bb0bec379e103cdcb
jlukenoff <jlukenoff@gmail.com>
2019-03-23 15:09:01 -0700

    Code clean up after successful test

--- relay_switch.js
+++ relay_switch.js
@@ -1,14 +1,17 @@
 const { Gpio } = require('onoff');
 
+// set up GPIO output to relay
 const relay = new Gpio(4, 'high');
 
+// get arguments from cli
 const timeOn = process.argv[2];
 
-console.log(timeOn);
+console.log('Time for relay to remain on: ', timeOn);
 
+// function to set relay to opposite of current state
 const toggleRelay = () => {
+  // get current state of relay
   const isOn = relay.readSync();
-  console.log(isOn);
   if (isOn === 1) {
     relay.writeSync(0);
     console.log('Relay turned on');
