3892406832f272edfe1bf868092cb30f553c4ba7
jlukenoff <jlukenoff@gmail.com>
2019-03-23 15:36:07 -0700

    Add console.logs

--- read_water_sensor.js
+++ read_water_sensor.js
@@ -3,6 +3,9 @@ const { Gpio } = require('onoff');
 const sensorOrangeWire = new Gpio(14, 'in', 'both');
 const sensorYellowWire = new Gpio(15, 'in', 'both');
 
+console.log(`Sensor Orange Wire State: ${sensorOrangeWire.readSync()}`);
+console.log(`Sensor Yellow Wire State: ${sensorYellowWire.readSync()}`);
+
 sensorOrangeWire.watch((e, val) => {
   if (e) console.error(`Error detected from ORANGE (GPIO14): ${e}`);
   console.log(`Event - ORANGE (GPIO14): ${val}`);
