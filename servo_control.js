aecdc1f345c73bc25f1bb341147a233bf4653786
John Lukenoff <jlukenoff@gmail.com>
2019-06-05 21:26:50 -0700

    test servo

--- servo_control.js
+++ servo_control.js
@@ -1,18 +1,35 @@
 const Gpio = require('pigpio').Gpio;
 
-const motorLeft = new Gpio(17, {mode: Gpio.OUTPUT});
+const motorLeft = new Gpio(17, { mode: Gpio.OUTPUT });
+const motorRight = new Gpio(22, { mode: Gpio.OUTPUT });
 
 let pulseWidth = 1000;
 let increment = 100;
 
-setInterval(() => {
-  motor.servoWrite(pulseWidth);
+// clockwise?
+motorLeft.servoWrite(1000);
+motorRight.servoWrite(1000);
 
-console.log(JSON.stringify(motor, null, 2));
-  pulseWidth += increment;
-  if (pulseWidth >= 2000) {
-    increment = -100;
-  } else if (pulseWidth <= 1000) {
-    increment = 100;
-  }
+setTimeout(() => {
+  // AntiClockwise?
+  motorLeft.servoWrite(2000);
+  motorRight.servoWrite(2000);
+  setTimeout(() => {
+    motorLeft.servoWrite(0);
+    motorRight.servoWrite(0);
+  }, 1000);
 }, 1000);
+const directions = ['FORWARD', 'RIGHT', 'BACK', 'left'];
+
+// setInterval(() => {
+//   motorLeft.servoWrite(pulseWidth);
+//   motorRight.servoWrite(pulseWidth);
+
+//   console.log(JSON.stringify(motorLeft, null, 2));
+//   pulseWidth += increment;
+//   if (pulseWidth >= 2000) {
+//     increment = -100;
+//   } else if (pulseWidth <= 1000) {
+//     increment = 100;
+//   }
+// }, 1000);
